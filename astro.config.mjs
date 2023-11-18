348784264e3dac5246cab521da3cf5b1b3d0f53e
KyleTryon <Kyle.Tryon@circleci.com>
2023-11-18 13:00:51 -0500

    feat: initialize repository

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4321"
+      : "https://techsquidtv.com",
+  integrations: [tailwind(), mdx(), sitemap(), react()],
+});
