797dcd154e341ceee9b768f6e707ba0bc30457d3
KyleTryon <Kyle.Tryon@circleci.com>
2023-11-27 23:01:24 -0500

    feat: customize tailwind shadows

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,6 +24,14 @@ export default {
         },
       },
     },
+    boxShadow: {
+      sm: "0 1px 2px 0 rgb(0 0 0/ 0.05)",
+      DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.25), 0 1px 2px -1px rgb(0 0 0 / 0.25)",
+      md: "0 1px 3px 0 rgb(0 0 0 / 0.25), 0 1px 2px -1px rgb(0 0 0 / 0.25)",
+      lg: "0 10px 15px -3px rgb(0 0 0 / 0.25), 0 4px 6px -4px rgb(0 0 0 / 0.25)",
+      xl: "0 20px 25px -5px rgb(0 0 0 / 0.25), 0 8px 10px -6px rgb(0 0 0 / 0.25)",
+      "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
+    },
     borderRadius: {
       none: "0",
       xs: "0.125rem",
