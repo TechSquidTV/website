e2027619abeec9b069f1a451c2d90f624da99318
KyleTryon <Kyle.Tryon@circleci.com>
2023-12-07 20:07:49 -0500

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,17 @@ import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://techsquidtv.com",
-  integrations: [tailwind(), sitemap(), react(), embeds(), mdx(), sentry(), spotlightjs()]
+  site:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4321"
+      : "https://techsquidtv.com",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    react(),
+    embeds(),
+    mdx(),
+    sentry(),
+    spotlightjs(),
+  ],
 });
