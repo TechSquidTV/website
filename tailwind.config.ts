ed8e31062d25e07357c5b1e85820e36cf3a7e6da
KyleTryon <Kyle.Tryon@circleci.com>
2023-12-15 22:21:33 -0500

    feat: remove custom shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,15 +35,6 @@ export default {
         },
       },
     },
-    boxShadow: {
-      sm: "0 1px 2px 0 rgb(0 0 0/ 0.05)",
-      DEFAULT:
-        "0 1px 3px 0 rgb(0 0 0 / 0.18), 0 1px 2px -1px rgb(0 0 0 / 0.18)",
-      md: "0 1px 3px 0 rgb(0 0 0 / 0.18), 0 1px 2px -1px rgb(0 0 0 / 0.18)",
-      lg: "0 10px 15px -3px rgb(0 0 0 / 0.18), 0 4px 6px -4px rgb(0 0 0 / 0.18)",
-      xl: "0 20px 25px -5px rgb(0 0 0 / 0.18), 0 8px 10px -6px rgb(0 0 0 / 0.18)",
-      "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.18)",
-    },
     borderRadius: {
       none: "0",
       xs: "0.125rem",
